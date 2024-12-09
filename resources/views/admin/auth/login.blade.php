71f2890d1067f4b6babc3bc82561962f50dbed7e
tsberecruit <tsberecruit@gmail.com>
2024-12-09 12:55:53 +0100

    Admin Panel Setup: Worked on the Admin login page layout - HTML&CSS

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,47 +1,73 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-    <h1 style="color: green">ADMIN LOGIN<h1>
-    <form method="POST" action="{{ route('admin.login') }}">
-        @csrf
+@extends('admin.auth.layouts.auth-master')
+@section('contents')
+<section class="section">
+    <div class="container mt-5">
+      <div class="row">
+        <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+          <div class="login-brand">
+            <img src="assets/img/stisla-fill.svg" alt="logo" width="100" class="shadow-light rounded-circle">
+          </div>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+          <div class="card card-primary">
+            <div class="card-header"><h4>Admin Login</h4></div>
+
+            <div class="card-body">
+                <!-- Session Status -->
+            <x-auth-session-status class="mb-4" :status="session('status')" />
+              <form method="POST" action="{{ route('admin.login') }}">
+                @csrf
+                <div class="form-group">
+                  <label for="email">Email</label>
+                  <input id="email" type="email" value="{{ old('email') }}" class="form-control {{  $errors->has('email') ? 'is-invalid' : '' }}" name="email" tabindex="1" required autofocus>
+                  
+                  <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                </div>
+
+                
+                <!-- Password -->
+                <div class="form-group">
+                  <div class="d-block">
+                      <label for="password" class="control-label">Password</label>
+                    <div class="float-right">
+                      <a href="{{ route('admin.password.request') }}" class="text-small">
+                        Forgot Password?
+                      </a>
+                    </div>
+                  </div>
+                  <input id="password" type="password" class="form-control" name="password" tabindex="2" required>
+                  <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                </div>
+
+
+
+                <!-- Remember Me -->
+                <div class="form-group">
+                  <div class="custom-control custom-checkbox">
+                    <input id="remember-me" type="checkbox" name="remember" class="custom-control-input" tabindex="3" >
+                    <label class="custom-control-label" for="remember-me">Remember Me</label>
+                  </div>
+                </div>
+
+                
+                
+
+                <div class="form-group">
+                  <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                    Login
+                  </button>
+                </div>
+              </form>
+              
+              
+
+            </div>
+          </div>
+          
+          <div class="simple-footer">
+            Copyright &copy; KellyXGlobal Tech Solutions {{ date('Y') }}
+          </div>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('admin.password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+      </div>
+    </div>
+  </section>
+@endsection
--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -10,10 +10,7 @@
   <link rel="stylesheet" href="{{ asset('admin/assets/modules/fontawesome/css/all.min.css') }}">
 
   <!-- CSS Libraries -->
-  <link rel="stylesheet" href="{{ asset('admin/assets/modules/jqvmap/dist/jqvmap.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('admin/assets/modules/weather-icon/css/weather-icons.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('admin/assets/modules/weather-icon/css/weather-icons-wind.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('admin/assets/modules/summernote/summernote-bs4.css') }}">
+ <link rel="stylesheet" href="{{ asset('admin/assets/modules/summernote/summernote-bs4.css') }}">
 
   <!-- Template CSS -->
   <link rel="stylesheet" href="{{ asset('admin/assets/css/style.css') }}">
@@ -32,10 +29,10 @@
       </div>
       <footer class="main-footer">
         <div class="footer-left">
-          Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
+          Copyright &copy; {{ date('Y') }} <div class="bullet"></div> Developed By <a href="#">KellyXGlobal Tech Solutions</a>
         </div>
         <div class="footer-right">
-          
+
         </div>
       </footer>
     </div>
@@ -49,18 +46,10 @@
   <script src="{{ asset('admin/assets/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
   <script src="{{ asset('admin/assets/modules/moment.min.js') }}"></script>
   <script src="{{ asset('admin/assets/js/stisla.js') }}"></script>
-  
-  <!-- JS Libraies -->
-  <script src="{{ asset('admin/assets/modules/simple-weather/jquery.simpleWeather.min.js') }}"></script>
-  <script src="{{ asset('admin/assets/modules/chart.min.js') }}"></script>
-  <script src="{{ asset('admin/assets/modules/jqvmap/dist/jquery.vmap.min.js') }}"></script>
-  <script src="{{ asset('admin/assets/modules/jqvmap/dist/maps/jquery.vmap.world.js') }}"></script>
-  <script src="{{ asset('admin/assets/modules/summernote/summernote-bs4.js') }}"></script>
-  <script src="{{ asset('admin/assets/modules/chocolat/dist/js/jquery.chocolat.min.js') }}"></script>
 
-  <!-- Page Specific JS File -->
-  <script src="assets/js/page/index-0.js"></script>
-  
+  <!-- JS Libraies -->
+  <script src="{{ asset('admin/assets/modules/summernote/summernote-bs4.js') }}"></script>
+ 
   <!-- Template JS File -->
   <script src="assets/js/scripts.js"></script>
   <script src="assets/js/custom.js"></script>
