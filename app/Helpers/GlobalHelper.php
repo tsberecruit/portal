abe7f9f880442cac3da2abf05720c795dafd282c
tsberecruit <tsberecruit@gmail.com>
2025-01-14 13:08:55 +0100

    Completing Admin sidebar activation

--- app/Helpers/GlobalHelper.php
+++ app/Helpers/GlobalHelper.php
@@ -1,8 +1,24 @@
 <?php
+
+
+
 /* Check input error */
 if(!function_exists('hasError')) {
     function hasError($errors, string $name) : ?string
     {
         return $errors->has($name) ? 'is-invalid' : '';
+    }
+}
+
+/** Set sidebar active */
+if(!function_exists('setSidebarActive')) {
+    function setSidebarActive(array $routes) : ?String
+    {
+        foreach($routes as $route) {
+            if(request()->routeIs($route)) {
+                return 'active';
+            }
+        }
+        return null;
     }
 }
